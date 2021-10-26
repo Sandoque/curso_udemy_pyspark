5334aacfebc7220c8c71b26a0544d81c6082f025
Antônio Carlos Sandoque <acsandoque@gmail.com>
2021-10-26 15:01:24 -0300

    dataframe - lendo arquivo csv

--- SRC/aula01.py
+++ SRC/aula01.py
@@ -1,6 +1,17 @@
 from pyspark.sql import SparkSession
 
-def main():
-
 if __name__ == "__main__":
-    main()
+    spark = (SparkSession.builder
+             .master("local[3]")
+             .appName("estudando-dataframes")
+             .getOrCreate())
+
+    df = (spark
+          .read
+          .format("csv")
+          .option("header", "true")
+          .option("inferSchema", "true")
+          .option("delimiter", ";")
+          .load("csv/arquivo_geral.csv"))
+
+    df.show(10)
